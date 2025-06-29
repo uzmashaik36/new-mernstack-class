1e8d93850dd9a1f14c50184ae1ac2bca90dedabb
UZMABEGUM SHAIK <144657854+uzmashaik36@users.noreply.github.com>
2025-06-30 00:08:24 +0530

    Add files via upload

---
+++ javascrtipt functions/index.js
@@ -0,0 +1,18 @@
+console.log("Javascript functions");
+function nice(name) {
+    console.log("Hey " + name + " your name is good!");
+    console.log("Hey " + name + " your are good!");
+    console.log("Hey " + name + " your dress is nice !");
+    console.log("Hey " + name + " your great!");
+
+}
+nice("uzma");
+nice("riya"); 
+
+function sum(a, b) {
+    // console.log(a+b);
+    return a + b
+}
+
+result = sum(5, 9);
+console.log("The sum of these numbers is:", result)
