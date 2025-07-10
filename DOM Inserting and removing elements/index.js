4ad2bf1e16d4e4b1bf794e6a68b92acf68846571
UZMABEGUM SHAIK <144657854+uzmashaik36@users.noreply.github.com>
2025-07-10 23:27:04 +0530

    Add files via upload

---
+++ DOM Inserting and removing elements/index.js
@@ -0,0 +1,30 @@
+console.log("hey");
+//document.querySelector(".container");
+//document.querySelector(".container").innerHTML
+
+//document.querySelector(".box");
+
+//document.querySelector(".box").innerHTML;
+
+//document.querySelector(".box").innerText;
+
+//document.querySelector(".container").outer;
+
+//document.querySelector(".container").outerHTML;
+
+//document.querySelector(".container").TagName;
+
+//document.querySelector(".container").nodeName;
+
+//ocument.querySelector(".container").textContent;
+
+//document.querySelector(".container").hidden = true;
+
+//document.querySelector(".box").innerHTML = "Hey I am jupiter"
+
+  // let div = document.createElement("div");
+            // div.innerHTML = "I have been inserted <b>by Uzma</b>"
+            // div.setAttribute("class", "created");
+            // document.querySelector(".container").before(div);
+            let cont = document.querySelector(".container")
+            cont.insertAdjacentHTML("beforebegin", "<b> help me </b>")
