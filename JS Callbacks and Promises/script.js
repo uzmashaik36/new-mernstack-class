e5ac783de87ac7e3c8ce2ce279cbebefdb2fea79
UZMA BEGUM SHAIK <144657854+uzmashaik36@users.noreply.github.com>
2025-07-27 23:59:22 +0530

    Add files via upload

--- JS Callbacks and Promises/script.js
+++ JS Callbacks and Promises/script.js
@@ -1,32 +1,20 @@
-console.log("Harry is a hacker")
-console.log("Rohan is a hecker")
-
-
-setTimeout(() => {
-    console.log("I am inside settimeout")
-}, 0);
+console.log("Vindhya is Hacker");
+console.log("riya is hecker");
 
 setTimeout(() => {
-    console.log("I am inside settimeout 2")
+   console.log("I am inside settimeout")
 }, 0);
+setTimeout(() => {
+   console.log("I am inside settimeout2")
+}, 2000);
 
 console.log("The End")
 
-const fn = () => {
-  console.log("Nothing")
-}
 
-const callback = (arg, fn) => {
-    console.log(arg)
-    fn()
-}
 
 const loadScript = (src, callback) => {
     let sc = document.createElement("script");
     sc.src = src;
-    sc.onload = callback("Harry", fn);
-    document.head.append(sc)
+    sc.onload = callback("vindhya")
+  
 }
-
-
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback )
